03a486bd2933b569cbff41f77919b867e80ab140
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2025-09-07 19:17:58 -0300

    2 Test for Chicago

--- TP_3/abstract_factory/tests/test_pizzas.py
+++ TP_3/abstract_factory/tests/test_pizzas.py
@@ -1,11 +1,18 @@
 import pytest
 from TP_3.abstract_factory.store import PizzaStore, NYPizzaStore, ChicagoPizzaStore
 
-def main():
-    print("This is a test module for pizzas.")
-    # Abstract Factory Pattern
-    print("NEw test for branch")
+
+def test_chicago_pizza_store():
+    chicago_store = ChicagoPizzaStore()
+    pizza = chicago_store.order_pizza("Cheese")
+    assert pizza.name == "Chicago Style Cheese Pizza"
+    assert pizza.dough.name == "Thick Crust Dough" 
+    assert "Plum Tomato Sauce" == pizza.sauce.name
 
 
-if __name__ == "__main__":
-    main()
+def test_chicago_clam_pizza():
+    chicago_store = ChicagoPizzaStore()
+    pizza = chicago_store.order_pizza("Clam")
+    assert pizza.name == "Chicago Style Clam Pizza"
+    assert pizza.dough.name == "Thick Crust Dough"  
+    assert "Plum Tomato Sauce" == pizza.sauce.name
