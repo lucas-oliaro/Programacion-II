137d8d8c3a1a97e9bfad835f620c3ead3ac29caf
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2025-09-07 18:39:13 -0300

    WIP Tests for TP3

---
+++ TP_3/abstract_factory/tests/test_pizzas.py
@@ -0,0 +1,10 @@
+from TP_3.abstract_factory.store import PizzaStore, NYPizzaStore, ChicagoPizzaStore
+
+def main():
+    print("This is a test module for pizzas.")
+    # Abstract Factory Pattern
+
+
+
+if __name__ == "__main__":
+    main()
