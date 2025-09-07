fd8a11c0560010a15d23709ec8209574cfb7c317
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2025-09-07 19:17:58 -0300

    2 Test for Chicago

--- TP_3/abstract_factory/tests/test_pizzas.py
+++ TP_3/abstract_factory/tests/test_pizzas.py
@@ -1,7 +1,11 @@
+import pytest
 from TP_3.abstract_factory.store import PizzaStore, NYPizzaStore, ChicagoPizzaStore
-from TP_3.abstract_factory.ingredients import Dough, NYPizzaStore, ChicagoPizzaStore
 
-def test_ny_cheese_pizza_has_correct_dough():
-    store = NYPizzaStore()
-    pizza = store.order_pizza("cheese")
-    assert isinstance(pizza.dough, Dough)
+def main():
+    print("This is a test module for pizzas.")
+    # Abstract Factory Pattern
+    print("NEw test for branch")
+
+
+if __name__ == "__main__":
+    main()
